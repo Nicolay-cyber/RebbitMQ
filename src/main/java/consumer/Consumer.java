27d6cc1dfa50dcc882116f0b64be3fd41d790f28
Nikolay <157130401678@mail.ru>
2022-04-08 15:39:23 +0300

    Initial commit

---
+++ src/main/java/consumer/Consumer.java
@@ -0,0 +1,11 @@
+package consumer;
+
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.sql.Connection;
+
+public class Consumer {
+    public static void main(String[] args) {
+
+    }
+}
